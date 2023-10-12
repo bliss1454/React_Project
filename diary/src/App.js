c20edbf4c975a2ca33d76983e1045529f20b0934
bliss1454 <rkgus1454@naver.com>
2023-10-12 19:48:12 +0900

    [FEAT] context추가

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,3 +1,4 @@
+import React, { useReducer, useRef } from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
@@ -6,21 +7,80 @@ import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 import New from './pages/New';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className='App'>
-        <h2>App.js</h2>
+const reducer = (state, action) => {
+  let newState = [];
+  switch(action.type){
+    case 'INIT' : {
+      return action.data;
+    }
+    case 'CREATE' : {
+      const newItem = {
+        ...action.data
+      };
+      newState = [newItem, ...state];
+      break;
+    }
+    case 'REMOVE': {
+      newState = state.filter((it)=> it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT': {
+      newState = state.map((it) => it.id === action.data.id? {...action.data}: it)
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
 
-        <img src={Process.env.PUBLIC URL + '/assets/happy.png'}/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/new' element={<New/>}/>
-          <Route path='/edit' element={<Edit/>}/>
-          <Route path='/diary/:id' element={<Diary/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+function App() {
+
+  const [data, dispatch] =useReducer(reducer,[]);
+  const dataId = useRef(0);
+  //CREATE
+  const onCreate = (date,content,emotion) => {
+    dispatch({type:"CREATE", data:{
+      id: dataId.current,
+      date: new Date(date).getTime(),
+      content,
+      emotion
+    }})
+    dataId.current+=1;
+  }
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({type:"REMOVE", targetId});
+  }
+  //EDIT
+  const onEdit = (targetId,date,content,emotion) => {
+    dispatch({type:"EDIT", data: {
+      id: targetId,
+      date: new Date(date).getTime(),
+      content,
+      emotion,
+    }})
+  }
+
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider 
+        value={{onCreate, onEdit, onRemove}} >
+      <BrowserRouter>
+        <div className='App'>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/new' element={<New/>}/>
+            <Route path='/edit' element={<Edit/>}/>
+            <Route path='/diary/:id' element={<Diary/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   )
 }
 
