c11362fd86e8c331ea9bd34c1ea65badcef01982
bliss1454 <rkgus1454@naver.com>
2023-10-13 19:18:22 +0900

    [Feat] diary edit 추가

--- diary/src/pages/Edit.js
+++ diary/src/pages/Edit.js
@@ -1,26 +1,32 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryDispatchContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
 
 const Edit = () => {
-
+    const [originData,setOriginData] = useState();
     const navigate = useNavigate();
+    const {id} = useParams();
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const id = searchParams.get('id');
-    console.log('id',id);
-    const mode = searchParams.get('mode');
-    console.log('mode',mode);
+    const diaryList = useContext(DiaryDispatchContext);
+
+    useEffect(() => {
+        if(diaryList.length >= 1 ) {
+            const targetDiary = diaryList.find(
+                (it) => parseInt(it.id) === parseInt(id)
+            );
+            
+            if(targetDiary) {
+                setOriginData(targetDiary);
+            } else {
+                navigate('/',{replace: true});
+            }
+        }
+    },[id,diaryList])
 
     return (
         <div>
-            <h1>Edit</h1>
-            <p>이곳은 일기 수정 페이지 입니다.</p>
-            <button onClick={()=> setSearchParams({ who: "winterlood"})}>QS 바꾸기</button>
-            <button onClick={()=> {
-                navigate('/home')
-            }}>이동하기</button>
-            <button onClick={()=>{
-                navigate(-1);
-            }}>뒤로가기</button>
+            {originData && <DiaryEditor isEdit={true} originData={originData} />}
         </div>
     )
 };
